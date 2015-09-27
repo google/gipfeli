36f9f04a0080aa9bf105ea1ff9cb20081a1f833f
Evan Nemerson <evan@nemerson.com>
2015-09-27 16:42:41 -0700

    Fix memory leak when sinks throw an exception
    This allows custom sink implementations which can throw an exception
    (for example, when there is insufficient space to store the output)
    to be used, while avoiding memory leaks which could occur in the event
    such an exception is thrown.

--- gipfeli-internal.cc
+++ gipfeli-internal.cc
@@ -268,7 +268,13 @@ size_t Gipfeli::CompressStream(
     }
 
     if (scratch_output == dest) {
-      writer->AppendMemBlock(new NewedMemBlock(scratch_output, output_size));
+      NewedMemBlock* new_block = new NewedMemBlock(scratch_output, output_size);
+      try {
+        writer->AppendMemBlock(new_block);
+      } catch (...) {
+        delete new_block;
+        throw;
+      }
       scratch_output = NULL;
       scratch_output_size = 0;
     } else {
