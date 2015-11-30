b20fb49a81ef274419fb00f2c201a7b843415fb0
Evan Nemerson <evan@nemerson.com>
2015-11-30 09:56:02 -0800

    Don't hardcode HAVE_BUILTIN_* macros in config.h
    This makes it possible to use a build system to detect support for
    the builtins, but also uses predefined macros as a fallback instead
    of just hardcoding the assumption that they exist.

--- config.h
+++ config.h
@@ -1,9 +1,21 @@
 /* Some basic macro definitions for high performance.
- * If something is not supported by your system, turn it off.
- * This will be substituted by automatic script in future.*/
+ * You can use a build system (such as autotools or cmake) to test for
+ * these features and define HAVE_* or NO_* to turn them on or off.
+ * Otherwise, attempt to determine support based on checking macros
+ * defined by the compiler and/or libc. */
 
-/* Define to 1 if the compiler supports __builtin_ctz and friends. */
-#define HAVE_BUILTIN_CTZ 1
+/* Define to 1 if the compiler supports __builtin_ctz and friends.
+   Clang and Intel both define __GNUC__ (and support __builtin_ctz and
+   __builtin_expect) */
+#if !defined(NO_BUILTIN_CTZ) && !defined(HAVE_BUILTIN_CTZ)
+#  if defined(__GNUC__)
+#    define HAVE_BUILTIN_CTZ 1
+#  endif
+#endif
 
 /* Define to 1 if the compiler supports __builtin_expect. */
-#define HAVE_BUILTIN_EXPECT 1
+#if !defined(NO_BUILTIN_EXPECT) && !defined(HAVE_BUILTIN_EXPECT)
+#  if defined(__GNUC__)
+#    define HAVE_BUILTIN_EXPECT 1
+#  endif
+#endif
